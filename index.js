b04ef4b8a5ad746ed9c15f9b0b62f340519bcb05
1067904955@qq.com <jiujiu123>
2018-05-04 15:02:57 +0800

    创意无敌 可惜我没有

--- index.js
+++ index.js
@@ -27,3 +27,62 @@
 ////    alert("断点"); 
 //    document.getElementById('main1112113').className = "main11123";
 //}
+//    document.getElementById('main3').className = "main31";
+//    var b = 1;
+//function moveboxin() {
+////    alert("进去了"); 
+//    var a = "main3";
+//    b++;
+//    var c = a+b;
+//   if (b >= 9) 
+//   {
+//       b = b-9;  
+//   }
+//    document.getElementById('main3').className = c;
+//    setInterval("moveboxin()",100);
+//}
+
+//function moveboxout() {
+////    alert("出来了"); 
+//    document.getElementById('main3').className = "main30";
+////    document.getElementById('main1112111').className = "main11121";
+//}
+
+//function changeState() {   
+//    moveboxout();
+//    setTimeout("changeState()",1000 );  
+//    moveboxin();
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
