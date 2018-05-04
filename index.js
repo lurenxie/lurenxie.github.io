fb44b9cfa308b3d0cc7c42b5e4e32a9bdff7f4aa
1067904955@qq.com <jiujiu123>
2018-05-04 10:45:20 +0800

    头部式样更新

---
+++ index.js
@@ -0,0 +1,29 @@
+function redbox1() {
+//    alert("断点"); 
+    document.getElementById('main1112111').className = "main111211";
+}
+
+function redbox11() {
+//    alert("断点"); 
+    document.getElementById('main1112111').className = "main11121";
+}
+
+function redbox2() {
+//    alert("断点"); 
+    document.getElementById('main1112112').className = "main111221";
+}
+
+function redbox22() {
+//    alert("断点"); 
+    document.getElementById('main1112112').className = "main11122";
+}
+
+function redbox3() {
+//    alert("断点"); 
+    document.getElementById('main1112113').className = "main111231";
+}
+
+function redbox33() {
+//    alert("断点"); 
+    document.getElementById('main1112113').className = "main11123";
+}
