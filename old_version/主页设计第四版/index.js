998f175d7a8587e42f4709e8d9b7b93153272486
1067904955@qq.com <jiujiu123>
2018-05-16 11:32:47 +0800

    把第四版主页暂时存放，将小红书作为主页进行展示

---
+++ old_version/主页设计第四版/index.js
@@ -0,0 +1,54 @@
+function f_gongju() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_gongju').style.display='block';
+	document.getElementById('u_shijie').style.display='none';
+	document.getElementById('u_shenmei').style.display='none';
+	document.getElementById('u_shuchu').style.display='none';
+	document.getElementById('m_intro').style.display='none';
+	document.getElementById('m_gongju_intro').style.display='block';
+	document.getElementById('u_gongju').style.background='#3c3c3c';	
+	document.getElementById('m_gongju').style.background='#3c3c3c';
+	document.getElementById('m_shijie').style.background='#555';
+	document.getElementById('m_shenmei').style.background='#555';
+	document.getElementById('m_shuchu').style.background='#555';
+}
+function f_shijie() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_shijie').style.display='block';
+	document.getElementById('u_gongju').style.display='none';
+	document.getElementById('u_shenmei').style.display='none';
+	document.getElementById('u_shuchu').style.display='none';
+	document.getElementById('u_shijie').style.background='#3c3c3c';	
+	document.getElementById('m_gongju').style.background='#555';
+	document.getElementById('m_shijie').style.background='#3c3c3c';
+	document.getElementById('m_shenmei').style.background='#555';
+	document.getElementById('m_shuchu').style.background='#555';
+}
+function f_shenmei() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_shenmei').style.display='block';
+	document.getElementById('u_gongju').style.display='none';
+	document.getElementById('u_shijie').style.display='none';
+	document.getElementById('u_shuchu').style.display='none';
+	document.getElementById('u_shenmei').style.background='#3c3c3c';
+	document.getElementById('m_gongju').style.background='#555';
+	document.getElementById('m_shijie').style.background='#555';
+	document.getElementById('m_shenmei').style.background='#3c3c3c';
+	document.getElementById('m_shuchu').style.background='#555';	
+}
+function f_shuchu() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_shuchu').style.display='block';
+	document.getElementById('u_gongju').style.display='none';
+	document.getElementById('u_shijie').style.display='none';
+	document.getElementById('u_shenmei').style.display='none';
+	document.getElementById('u_shuchu').style.background='#3c3c3c';	
+	document.getElementById('m_gongju').style.background='#555';
+	document.getElementById('m_shijie').style.background='#555';
+	document.getElementById('m_shenmei').style.background='#555';
+	document.getElementById('m_shuchu').style.background='#3c3c3c';
+}
