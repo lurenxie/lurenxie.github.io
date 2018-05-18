caf2baf5ce983dbb302fa605319b9b7d9f9bb0f6
1067904955@qq.com <jiujiu123>
2018-05-18 16:39:34 +0800

    虽然失去了斗志，但还是会坚持，总不能就这么结束了，对吧

--- index.js
+++ index.js
@@ -1,62 +1,54 @@
-/*
-* @Author: Ding Jianlong
-* @Date:   2018-05-16 08:59:57
-* @Last Modified by:   Ding Jianlong
-* @Last Modified time: 2018-05-16 16:36:22
-*/
-function body_main() {
+function f_gongju() {
 	// alert("断点");
-	var autocontent = new Array()
-	autocontent[0] = "我";
-	autocontent[1] = "我的";
-	autocontent[2] = "我的种";
-	autocontent[3] = "我的种草";
-	autocontent[4] = "我的种草基";
-	autocontent[5] = "我的种草基地";
-	autocontent[6] = "剁";
-	autocontent[7] = "剁手";
-	autocontent[8] = "剁手神";
-	autocontent[9] = "剁手神器";
-	autocontent[10] = "手";
-	autocontent[11] = "手机";
-	autocontent[12] = "手机里";
-	autocontent[13] = "手机里的";
-	autocontent[14] = "手机里的随";
-	autocontent[15] = "手机里的随身";
-	autocontent[16] = "手机里的随身手";
-	autocontent[17] = "手机里的随身手账";
-	var i = 0;
-	function title_auto_js() {
-		// alert("断点");
-		document.getElementById('title_auto_con').innerHTML = autocontent[i];
-		document.getElementById("title_auto_con").innerHTML += "|";
-		i++;
-		if (i == 18) { i = 0 ; }
-	}
-
-	var m = 0;
-	var pg1=document.getElementById('pg1');
-	function pg_auto() { 
-		if(pg1.value!=100) 
-			pg1.value++; 
-		else 
-			pg1.value=0;
-		document.getElementById('red_att').style.fontSize = '50px';
-		document.getElementById('red_att').style.fontWeight = 'bold';
-		document.getElementById('pg1').style.width = '280px';
-	}
-	setInterval(pg_auto,15);
-	
-
-
-
-
-
-
-
-
-	
-	setInterval(title_auto_js,300);
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_gongju').style.display='block';
+	document.getElementById('u_shijie').style.display='none';
+	document.getElementById('u_shenmei').style.display='none';
+	document.getElementById('u_shuchu').style.display='none';
+	document.getElementById('m_intro').style.display='none';
+	document.getElementById('m_gongju_intro').style.display='block';
+	document.getElementById('u_gongju').style.background='#3c3c3c';	
+	document.getElementById('m_gongju').style.background='#3c3c3c';
+	document.getElementById('m_shijie').style.background='#555';
+	document.getElementById('m_shenmei').style.background='#555';
+	document.getElementById('m_shuchu').style.background='#555';
 }
-
-
+function f_shijie() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_shijie').style.display='block';
+	document.getElementById('u_gongju').style.display='none';
+	document.getElementById('u_shenmei').style.display='none';
+	document.getElementById('u_shuchu').style.display='none';
+	document.getElementById('u_shijie').style.background='#3c3c3c';	
+	document.getElementById('m_gongju').style.background='#555';
+	document.getElementById('m_shijie').style.background='#3c3c3c';
+	document.getElementById('m_shenmei').style.background='#555';
+	document.getElementById('m_shuchu').style.background='#555';
+}
+function f_shenmei() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_shenmei').style.display='block';
+	document.getElementById('u_gongju').style.display='none';
+	document.getElementById('u_shijie').style.display='none';
+	document.getElementById('u_shuchu').style.display='none';
+	document.getElementById('u_shenmei').style.background='#3c3c3c';
+	document.getElementById('m_gongju').style.background='#555';
+	document.getElementById('m_shijie').style.background='#555';
+	document.getElementById('m_shenmei').style.background='#3c3c3c';
+	document.getElementById('m_shuchu').style.background='#555';	
+}
+function f_shuchu() {
+	// alert("断点");
+	// document.getElementById("target_gongju").innerHTML.display = "block";
+	document.getElementById('u_shuchu').style.display='block';
+	document.getElementById('u_gongju').style.display='none';
+	document.getElementById('u_shijie').style.display='none';
+	document.getElementById('u_shenmei').style.display='none';
+	document.getElementById('u_shuchu').style.background='#3c3c3c';	
+	document.getElementById('m_gongju').style.background='#555';
+	document.getElementById('m_shijie').style.background='#555';
+	document.getElementById('m_shenmei').style.background='#555';
+	document.getElementById('m_shuchu').style.background='#3c3c3c';
+}
